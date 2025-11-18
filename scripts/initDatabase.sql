a64061f2b19984bc0ac95d3b9d15a5680121f24f
rawanelhawary <164145388+rawanelhawary@users.noreply.github.com>
2025-11-18 08:27:42 +0200

    Update initDatabase.sql

--- scripts/initDatabase.sql
+++ scripts/initDatabase.sql
@@ -1,1 +1,21 @@
+-- initialization a data warehouse 
+use dataWarehouse 
+go 
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name='DataWarehouse')
+ BEGIN
+ ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACk IMMEDIATE
+ DROP DATABASE DataWarehouse
+   END
+   go ;
+
+--return a mlti user if you want 
+--ALTER DATABASE DataWarehouse SET MULTI_USER WITH ROLLBACK IMMEDIATE;
+
+use DataWarehouse 
+go
+create schema bronze;
+go
+create schema sliver;
+go
+create schema gold;
 
